aabd5990cd0541bd17c956dc3419925dad324d91
Anna Yundina <annayundina@Annas-MacBook-Pro.local>
2020-11-04 20:55:03 +0100

    Use toupper()

--- day00/ex00/megaphone.cpp
+++ day00/ex00/megaphone.cpp
@@ -1,30 +1,22 @@
 #include <iostream>
-#include <string>
-
-std::string stringToUppercase(std::string argument)
-{
-  std::string output;
-  int string_size = argument.size();
-  for (int i = 0; i < string_size; i++)
-  {
-    if (argument[i] >= 'a' && argument[i] <= 'z')
-      output.push_back(argument[i] - ' ');
-    else
-      output.push_back(argument[i]);
-  }
-  return output;
-}
+#include <cctype>
 
 int main(int argc, char **argv)
 {
-  std::string argument;
-  if (argc > 1)
-  {
-    for (int i = 1; i < argc; i++)
-      std::cout << stringToUppercase(argv[i]);
-    std::cout << std::endl;
-  }
-  else
-    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-  return 0;
+	char c;
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j] != '\0'; j++)
+			{
+				c = toupper(argv[i][j]);
+				std::cout << c;
+			}
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return 0;
 }
